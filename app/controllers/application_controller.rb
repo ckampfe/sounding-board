b1687948b165ea4df88ea4da28d49c75519021af
Apprentice <apprentice@dbc-chicago031.local>
2013-11-20 17:31:51 -0600

    added current_user= helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
       User.find_by(id: session[:current_user_id])
   end
 
+  def current_user=(user)
+    session[:current_user_id] = user.id
+  end
+
 end
