ac03f2d124fc380392cbbeb16285beb44d1ffc7e
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-21 14:13:15 -0600

    broke out session identification into helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include ControllerHelper
+
 
   def index
   end
 
-  protected
-  def current_user
-    @current_user ||= session[:current_user_id] &&
-      User.find_by(id: session[:current_user_id])
-  end
-
-  def current_user_assign(user)
-    session[:current_user_id] = user.id
-  end
-
-  def logout_user
-    session[:current_user_id] = nil
-  end
+  
 
 end
