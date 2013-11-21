d855fa2dafa01b09af68a3b405cfaa290170c8be
Apprentice <apprentice@dbc-chicago031.local>
2013-11-21 11:05:00 -0600

    Added NOT NULL constraint to short_url and title in Song migration

--- db/migrate/20131120210326_create_songs.rb
+++ db/migrate/20131120210326_create_songs.rb
@@ -1,8 +1,8 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :short_url
-      t.string :title
+      t.string :short_url, :null => false
+      t.string :title, :null => false
       t.string :artist
       t.string :album
       t.integer :year
