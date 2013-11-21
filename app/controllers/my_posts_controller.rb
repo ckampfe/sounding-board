2735bd1b9d16664277f8d9d22984f561e304ac6a
Apprentice <apprentice@dbc-chicago031.local>
2013-11-21 17:30:05 -0600

    Added controller to display all posts made or commented on by current_user

--- app/controllers/my_posts_controller.rb
+++ app/controllers/my_posts_controller.rb
@@ -2,11 +2,10 @@ class MyPostsController < ApplicationController
 
   def index
     my_created_posts = current_user.posts
-
     my_posts_commented_on = current_user.posts_commented_on
+    @my_posts = [my_created_posts, my_posts_commented_on].flatten!
 
-    @my_posts = [my_created_posts, my_posts_commented_on].flatten.uniq!
-    render :file => "my_posts"
+    render :file => :my_posts
   end
 
 end
