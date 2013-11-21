5eca53c4c1754561cb23581d6b950d9f30331691
Apprentice <apprentice@dbc-chicago031.local>
2013-11-21 10:44:55 -0600

    Added mass assignment check

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,7 +14,38 @@ describe Post do
 
   context "#save" do
 
+    it "valid posts can be saved successfully" do
+      post.save!
+
+      Post.find(post.id).should be_an_instance_of Post
+    end
+
+    it "will not allow a null title" do
+      post.title = nil
+      expect { post.save! }.to raise_error(ActiveRecord::StatementInvalid)
+    end
+
+    it "will not allow a null user_id" do
+      post.user_id = nil
+      expect { post.save! }.to raise_error(ActiveRecord::StatementInvalid)
+    end
+
+    it "will not allow a null song_id" do
+      post.song_id = nil
+      expect { post.save! }.to raise_error(ActiveRecord::StatementInvalid)
+    end
+
+    it "will allow a null motivation" do
+      post.motivation = nil
+      expect { post.save! }.to_not raise_error(ActiveRecord::StatementInvalid)
+    end
+
   end
+
+  it { should allow_mass_assignment_of(:title) }
+  it { should allow_mass_assignment_of(:motivation) }
+  it { should allow_mass_assignment_of(:user_id) }
+  it { should allow_mass_assignment_of(:song_id) }
 
 
 end
