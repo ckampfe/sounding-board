c281b9e7954b3e6822720cff40e271f6cbcb4f0d
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 18:40:55 -0600

    Seedfile created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  User.create!(
+               :name     => Faker::Internet.user_name,
+               :email    => Faker::Internet.email,
+               :password => "jams"
+              )
+end
+
+users_length = User.all.count
+
+10.times do
+  song = Song.create!(
+               :short_url => Faker::Internet.url,
+               :title     => Faker::Lorem.words(3),
+               :artist    => Faker::Lorem.words(2),
+               :album     => Faker::Lorem.words(3),
+               :year      => rand(1950..2013)
+              )
+
+  song_id = song.id
+
+  post = Post.create!(
+               :title         => Faker::Lorem.words(3),
+               :motivation    => Faker::Lorem.words(7),
+               :user_id       => rand(users_length),
+               :song_id       => song_id
+              )
+
+  post_id = post.id
+
+  comments_number = rand(5) + 1
+
+  comments_number.times do
+    comment = Comment.create!(
+                    :user_id => rand(users_length),
+                    :body    => Faker::Lorem.words(10),
+                    :song_id => song_id,
+                    :post_id => post_id
+                   )
+
+    comment_id = comment.id
+
+    rand(users_length * 2).times do
+      CommentVote.create!(
+                          :user_id => rand(users_length),
+                          :comment_id => comment_id,
+                          :is_up_vote => rand(2) > 0 ? true : false
+                         )
+      end
+  end
+end
