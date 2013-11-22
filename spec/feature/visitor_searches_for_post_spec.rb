41b5c683a0bcd2f47e59c3aede10357607dc7e6c
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-22 16:28:59 -0600

    passing logout integration test

--- spec/feature/visitor_searches_for_post_spec.rb
+++ spec/feature/visitor_searches_for_post_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
-feature "User signs up" do
+feature "Visitor searches for post" do
+
   scenario "when signing up" do
+
     visit "/users/new"
     fill_in "Name", :with => "jill"
     fill_in "Email", :with => "skylar@corkerykaulke.org"
     fill_in "Password", :with => "jams"
     click_button "signup"
-    expect(page).to have_text("My Post")
+
+      expect(page).to have_text("My Post")
   end
 end
 
