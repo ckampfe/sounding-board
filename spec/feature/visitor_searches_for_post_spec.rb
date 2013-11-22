d5b8b0e7989512de5b98b49b83659f807aa3f47a
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-22 16:41:00 -0600

    passing integration test for visitor search post functionality

--- spec/feature/visitor_searches_for_post_spec.rb
+++ spec/feature/visitor_searches_for_post_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 feature "Visitor searches for post" do
-
-  scenario "when signing up" do
-
-    visit "/users/new"
-    fill_in "Name", :with => "jill"
-    fill_in "Email", :with => "skylar@corkerykaulke.org"
-    fill_in "Password", :with => "jams"
-    click_button "signup"
-
-      expect(page).to have_text("My Post")
+  scenario "when searching for post" do
+    user = User.create(name: 'clark', email: "clark@gmail.com", password: 'jams')
+    song = Song.create(title: 'Best hits of Enya', album: 'Enya Does it Again', short_url: "google.com")
+    post = Post.create(title: 'clark rocks it', motivation: "I just love listening to myself rock it.  What can I say.", user_id: user.id, song_id: song.id)
+    visit "/"
+    fill_in "Search", :with => "clark rocks it"
+    click_button "search"
+    expect(page).to have_text("love")
   end
 end
 
