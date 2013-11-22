ea998ed2415688856a8a83a15f6bd2eef3431a6c
Apprentice <apprentice@dbc-chicago031.local>
2013-11-22 11:30:59 -0600

    Refactored PostsController's show method to utilize get_song_from_rdio method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,13 +61,8 @@ class PostsController < ApplicationController
     @comments = @post.comments
     # @song = @post.song # NYI because song requires Rdio to be setup first
     # FAKING A SONG
-    @song = Song.find_or_create_by_title(
-        :title => "This is a faked song",
-        :short_url => "http://www.google.com",
-        :artist => "Faked artist",
-        :album => "Faked Album",
-        :year => 1000
-      )
+    @song = get_song_from_rdio("FAKE!!!")
+    @song.save
 
   end
 
