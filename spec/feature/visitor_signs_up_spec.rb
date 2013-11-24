d95f08c910f9c6a2e79f0cdfb0d3739a56cbe85a
Nathan Hadlock <hadlocna@gmail.com>
2013-11-24 08:50:04 -0600

    changed signup integration test to fit new formatting

--- spec/feature/visitor_signs_up_spec.rb
+++ spec/feature/visitor_signs_up_spec.rb
@@ -10,12 +10,12 @@ feature "User signs up" do
 
   scenario "when signing up with no email" do
     sign_up_with("name","", "12223")
-    expect(page).to have_text("Email:")
+    expect(page).to have_text("Email")
   end
 
   scenario "when signing up with no password" do
     sign_up_with("name","name@email.com", "")
-    expect(page).to have_text("Email:")
+    expect(page).to have_text("Email")
   end
 
   def sign_up_with(name, email, password)
